63afb1e90a0126546d045406753fc0b9243a80c3
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-09-03 23:08:40 +0300

    Homework 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryCallback = ({ preview, original, description }) => {
+    return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+};
+
+const galleryElement = document.querySelector('ul.gallery');
+const galleryHtml = galleryItems.map(galleryCallback).join('');
+
+galleryElement.insertAdjacentHTML('beforeend', galleryHtml);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
