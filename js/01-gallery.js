fcd8c2061ed738e3a160af7c57eeb7b9950f27ac
OksanaT23 <129532941+OksanaT23@users.noreply.github.com>
2023-09-03 21:38:30 +0300

    Homework 7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryCallback = ({ preview, original, description }) => {
+    return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+};
+
+const galleryElement = document.querySelector('ul.gallery');
+const galleryHtml = galleryItems.map(galleryCallback).join('');
+
+galleryElement.insertAdjacentHTML('beforeend', galleryHtml);
+
+const galleryItemLinkCallback = event => {
+    event.preventDefault();
+
+    const galleryItemLink = event.currentTarget;
+    const previewImage = galleryItemLink.querySelector('img.gallery__image');
+    const originalUrl = previewImage.dataset.source;
+    const instance = basicLightbox.create(`
+        <img src="${originalUrl}" alt="${previewImage.alt}" title="${previewImage.alt}">
+    `);
+
+    instance.show();
+};
+
+const galleryLinks = document.querySelectorAll('ul.gallery a.gallery__link');
+
+galleryLinks.forEach(linkElement => {
+    linkElement.addEventListener('click', galleryItemLinkCallback);
+});
+
